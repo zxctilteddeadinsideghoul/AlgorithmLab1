8aea384c386f92c5dd204db0a1cb121db735235c
sadtowa <toni.afanasev.0505@mail.ru>
2024-09-24 20:53:32 +0500

    native power algorithm done

---
+++ algorithms/powAlgs/NaivePow.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlgoritmLab1.algorithms.templates;
+
+
+namespace AlgoritmLab1.algorithms.powAlgs
+{
+    internal class NaivePow : PowAlg
+    {
+        protected override void DoAlg(object data)
+        {
+            uint[] d = (uint[])data;
+
+            uint x = d[0];
+            uint n = d[1];
+
+            int f = 1;
+            int k = 0;
+            while (k < n)
+            {
+                f *= k;
+                k++;
+            }
+            
+        }
+    }
+}
--- algorithms/templates/PowAlg.cs
+++ algorithms/templates/PowAlg.cs
@@ -13,7 +13,7 @@ namespace AlgoritmLab1.algorithms.templates
         protected override object GetData(uint n)
         {
             //тут я возвращаю массив из двух элементов в порядке: [0] - число для возведения в степень [1] - степень
-            return new uint[2] { (uint)rand.Next(), n };
+            return new uint[2] { (uint)rand.Next(100), n };
         }
     }
 }
