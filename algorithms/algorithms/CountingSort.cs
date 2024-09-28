e29577ab3b98444ea6c4e5a4a3eb56b7387e8c7b
sadtowa <toni.afanasev.0505@mail.ru>
2024-09-28 20:38:13 +0500

    Counting sort alg

---
+++ algorithms/algorithms/CountingSort.cs
@@ -0,0 +1,57 @@
+﻿using AlgoritmLab1.algorithms.templates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace AlgoritmLab1.algorithms.algorithms
+{
+    internal class CountingSort : Algorithm
+    {
+        protected override void DoAlg(object data)
+        {
+            uint[] array = (uint[])data;
+
+            if (array.Length == 0)
+            {
+                return;
+            }
+
+            
+            uint max = array[0];
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] > max)
+                    max = array[i];
+            }
+
+            
+            uint[] countArray = new uint[max + 1];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                countArray[array[i]]++;
+            }
+
+            for (int i = 1; i < countArray.Length; i++)
+            {
+                countArray[i] += countArray[i - 1];
+            }
+
+            uint[] outputArray = new uint[array.Length];
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                outputArray[countArray[array[i]] - 1] = array[i];
+                countArray[array[i]]--;
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = outputArray[i];
+            }
+        }
+    }
+}
